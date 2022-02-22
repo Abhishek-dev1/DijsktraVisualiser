45c95cd16d586c7f32ad739d9718efa6f6e04667
Abhishek <88185067+Abhishek-dev1@users.noreply.github.com>
2022-02-22 10:15:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import PathVisualizer from './PathVisualize/PathVisualizer';
+function App() {
+  return (
+    <div className="App">
+      <PathVisualizer></PathVisualizer>
+    </div>
+  );
+}
+
+export default App;
